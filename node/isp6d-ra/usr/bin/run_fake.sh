37afd239bae08e179a7a3acdb7c9133f2726ff18
Markus Stenberg <markus.stenberg@iki.fi>
2013-07-17 17:02:41 +0200

    Added second server to both DHCPv4 and DHCPv6 results. And it resulted in finding few bugs.

--- node/isp6d-ra/usr/bin/run_fake.sh
+++ node/isp6d-ra/usr/bin/run_fake.sh
@@ -8,8 +8,8 @@
 # Copyright (c) 2013 cisco Systems, Inc.
 #
 # Created:       Thu Feb  7 13:57:59 2013 mstenber
-# Last modified: Wed May 22 14:27:16 2013 mstenber
-# Edit time:     12 min
+# Last modified: Wed Jul 17 16:58:20 2013 mstenber
+# Edit time:     13 min
 #
 
 . /usr/bin/luaenv.sh
@@ -19,6 +19,7 @@ mkdir -p $LOGDIR
 ENABLE_MST_DEBUG=1 lua $CORE/fakedhcpv6d.lua \
   --join=eth0 \
   --dns=2000::2 \
+  --dns=2001:100::1 \
   --search=v6.lab.example.com \
   --pref=3000 \
   --valid=4000 \
