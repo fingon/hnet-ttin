413751651e9440a8abe1453ddbac01c33f16d42d
Markus Stenberg <markus.stenberg@iki.fi>
2013-10-28 12:34:28 +0200

    Updated the summarization script to handle router ids instead of addresses.

--- util/summarize_bird_neigh_changes.py
+++ util/summarize_bird_neigh_changes.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2013 cisco Systems, Inc.
 #
 # Created:       Thu Feb 28 12:39:13 2013 mstenber
-# Last modified: Wed Jun  5 09:19:37 2013 mstenber
-# Edit time:     35 min
+# Last modified: Mon Oct 28 12:32:21 2013 mstenber
+# Edit time:     61 min
 #
 """
 
@@ -29,11 +29,9 @@ adjacency shown in rather compact fashion
 
 import re
 
-add_neigh_re = re.compile('^(?P<t>.*) <TRACE>.*New neighbor found: (?P<addr>.*) on (?P<ifname>.*)\s*$').match
+add_neigh_re = re.compile('^(?P<t>.*) <TRACE>.*New neighbor found: \S+ / (?P<addr>.*) on (?P<ifname>.*)\s*$').match
 
-remove_neigh_re = re.compile('^(?P<t>.*) <TRACE>.*Removing neighbor (?P<addr>.*)\s*$').match
-
-remove_neigh_re2 = re.compile('^(?P<t>.*) <TRACE>.*Neighbor (?P<addr>.*) changes state.*full.*init.*$').match
+remove_neigh_re = re.compile('^(?P<t>.*) <TRACE>.*Removing neighbor \S+ / (?P<addr>.*)\s*$').match
 
 reconf_re = re.compile('^(?P<t>.*) <INFO> Reconfiguring').match
 
@@ -55,10 +53,10 @@ class Node:
             self.neigh[addr] = [1, ifname]
         self.timeline.append((t, 'add', addr, ifname))
     def remove_neigh(self, t, addr):
+        assert self.neigh.has_key(addr), 'no %s for %s in %s' % (addr, self.name, self.neigh)
         v = self.neigh[addr]
         v[0] = v[0] - 1
         if v[0] == 0:
-            #assert self.neigh.has_key(addr), addr
             del self.neigh[addr]
         self.timeline.append((t, 'remove', addr))
     def peek(self):
@@ -68,6 +66,8 @@ class Node:
         t = self.timeline[0]
         del self.timeline[0]
         return t
+    def count(self):
+        return len(self.neigh)
 
 def analyze_files(filelist):
     nodes = []
@@ -89,15 +89,14 @@ def analyze_files(filelist):
                 gr = m.groupdict()
                 #print 'match remove 1', gr
                 node.remove_neigh(gr['t'], gr['addr'])
-            m = remove_neigh_re2(line)
-            if m is not None and False:
-                gr = m.groupdict()
-                #print 'match remove 2', gr
-                node.remove_neigh(gr['t'], gr['addr'])
+
             m = reconf_re(line)
             if m is not None:
                 gr = m.groupdict()
-                node.reconfigure(gr['t'])
+
+                # reconfigure does NOT apparently kill neighbor
+                # relations anymore
+                #node.reconfigure(gr['t'])
         nodes.append(node)
         i = i + 1
     while 1:
@@ -114,6 +113,10 @@ def analyze_files(filelist):
             break
         n.pop()
         print n.name, t
+    c = 0
+    for node in nodes:
+        c = c + node.count()
+    print 'Total of %d neighbor relations among %d nodes' % (c, len(nodes))
 
 if __name__ == '__main__':
     import sys
