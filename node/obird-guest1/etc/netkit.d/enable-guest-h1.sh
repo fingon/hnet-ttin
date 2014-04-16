346ba8002816903ebb556aac0e077949ef378783
Markus Stenberg <markus.stenberg@iki.fi>
2014-04-16 15:07:15 +0300

    Added obird7-guest topology (which has client behind guest interface) and obird-guest1 template (with eth1 being guest interface).

---
+++ node/obird-guest1/etc/netkit.d/enable-guest-h1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ue
+#-*-sh-*-
+#
+# $Id: enable-guest-h1.sh $
+#
+# Author: Markus Stenberg <mstenber@cisco.com>
+#
+# Copyright (c) 2014 cisco Systems, Inc.
+#
+# Created:       Wed Apr 16 13:50:23 2014 mstenber
+# Last modified: Wed Apr 16 15:51:01 2014 mstenber
+# Edit time:     3 min
+#
+
+uci set network.h1.guest=1
+uci commit network
+/etc/init.d/network restart
