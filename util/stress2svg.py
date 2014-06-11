a354367724c183e7013bbe20c262381bc20ec347
Markus Stenberg <markus.stenberg@iki.fi>
2014-06-11 14:08:31 +0300

    Some tuning of the graph appearance - added labels for dates etc.

--- util/stress2svg.py
+++ util/stress2svg.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2014 cisco Systems, Inc.
 #
 # Created:       Wed Jun 11 12:57:29 2014 mstenber
-# Last modified: Wed Jun 11 13:40:29 2014 mstenber
-# Edit time:     14 min
+# Last modified: Wed Jun 11 14:07:40 2014 mstenber
+# Edit time:     22 min
 #
 """
 
@@ -24,6 +24,23 @@ import nosefails
 import glob
 import argparse
 
+def uniquify(l):
+    # Remove shared substrings
+    firsts = [x[0] for x in l]
+    diff = [c for c in firsts if c != firsts[0]]
+    if diff:
+        return l
+    # Can remove first character
+    nexts = [x[1:] for x in l]
+    return uniquify(nexts)
+
+def startify(l):
+    s = ''
+    for e in l:
+        if e[0] != s:
+            s = e[0]
+            yield e
+
 ap = argparse.ArgumentParser()
 ap.add_argument('directory',
                 nargs='+',
@@ -33,7 +50,9 @@ args = ap.parse_args()
 results = list(nosefails.parse_logs(*glob.glob('%s/log*.txt' % dir)) for dir in args.directory)
 
 style = Style(colors=('#00ff00', '#00aa00', '#ff4000', '#ff0000'))
-c = pygal.StackedBar(style=style)
+c = pygal.StackedBar(style=style,  show_minor_x_labels=False, truncate_label=11)
+c.x_labels = uniquify(args.directory)
+c.x_labels_major = list(startify(c.x_labels))
 c.title = 'Stress test results over time'
 c.add('success', [r.cases_ok() for r in results])
 c.add('flaky', [r.cases_fail('flaky') for r in results])
