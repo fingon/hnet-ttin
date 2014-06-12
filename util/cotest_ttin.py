4b97b60b2a99f95ca44749656ee4693ebcc79aeb
Markus Stenberg <markus.stenberg@iki.fi>
2014-06-12 13:45:49 +0300

    Allow for 3 repeated PCPPings.

--- util/cotest_ttin.py
+++ util/cotest_ttin.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2014 cisco Systems, Inc.
 #
 # Created:       Tue Mar 25 10:39:18 2014 mstenber
-# Last modified: Tue Jun 10 17:57:54 2014 mstenber
-# Edit time:     488 min
+# Last modified: Thu Jun 12 11:26:31 2014 mstenber
+# Edit time:     490 min
 #
 """
 
@@ -489,12 +489,12 @@ SOCAT_LISTEN_FORMAT='/usr/bin/socat -v %s,reuseaddr,fork exec:"/bin/cat"'
 base_4_remote_pcp_test = [
     nodeStart('client', SOCAT_LISTEN_FORMAT % 'tcp4-l:10000'),
     cotest.RepeatStep(nodePCPMap('client', '-4 -p 10000 -T'), wait=1, times=3),
-    nodePCPPing('server', 'client', 'tcp4'),
+    cotest.RepeatStep(nodePCPPing('server', 'client', 'tcp4'), wait=1, times=3),
     nodeKill('client', '/usr/bin/socat'),
 
     nodeStart('client', SOCAT_LISTEN_FORMAT % 'udp4-l:10001'),
     nodePCPMap('client', '-4 -p 10001 -U'),
-    nodePCPPing('server', 'client', 'udp4'),
+    cotest.RepeatStep(nodePCPPing('server', 'client', 'udp4'), wait=1, times=3),
     nodeKill('client', '/usr/bin/socat'),
     ]
 
@@ -553,7 +553,7 @@ base_6_remote_ping_test = [
 base_6_remote_pcp_test = [
     nodeStart('client', SOCAT_LISTEN_FORMAT % 'tcp6-l:10002'),
     cotest.RepeatStep(nodePCPMap('client', '-6 -p 10002 -T'), wait=1, times=3),
-    nodePCPPing('server', 'client', 'tcp6'),
+    cotest.RepeatStep(nodePCPPing('server', 'client', 'tcp6'), wait=1, times=3),
     nodeKill('client', '/usr/bin/socat'),
 
     # N/A due to breakage with multiple ISPs; oh well
