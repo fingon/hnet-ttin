770ea8d39fd58bed7fe88e5061d6e6b0bfd102ef
Markus Stenberg <markus.stenberg@iki.fi>
2014-06-16 12:05:03 +0300

    Found one more place with network restart instead of reload -> should address adhoc case fails.

--- node/owrt-adhoc/etc/netkit.d/enable-adhoc-all.sh
+++ node/owrt-adhoc/etc/netkit.d/enable-adhoc-all.sh
@@ -8,7 +8,7 @@
 # Copyright (c) 2014 cisco Systems, Inc.
 #
 # Created:       Fri Jun  6 08:41:45 2014 mstenber
-# Last modified: Fri Jun  6 08:42:06 2014 mstenber
+# Last modified: Mon Jun 16 12:04:44 2014 mstenber
 # Edit time:     1 min
 #
 
@@ -17,4 +17,7 @@ uci set network.h1.adhoc=1
 uci set network.h2.adhoc=1
 uci set network.h3.adhoc=1
 uci commit network
-/etc/init.d/network restart
+
+#/etc/init.d/network restart
+#restart frequently bugs, should use reload (cyrusff, 20140612)
+/etc/init.d/network reload
