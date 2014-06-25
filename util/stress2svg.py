4c13a8dacb12c306eecf5778d076d45c5512e8ab
Markus Stenberg <markus.stenberg@iki.fi>
2014-06-25 12:32:55 +0300

    Made the colors more distinctive + names of failure modes bit more descriptive.

--- util/stress2svg.py
+++ util/stress2svg.py
@@ -9,8 +9,8 @@
 # Copyright (c) 2014 cisco Systems, Inc.
 #
 # Created:       Wed Jun 11 12:57:29 2014 mstenber
-# Last modified: Wed Jun 11 14:07:40 2014 mstenber
-# Edit time:     22 min
+# Last modified: Tue Jun 24 18:55:47 2014 mstenber
+# Edit time:     26 min
 #
 """
 
@@ -49,15 +49,15 @@ args = ap.parse_args()
 
 results = list(nosefails.parse_logs(*glob.glob('%s/log*.txt' % dir)) for dir in args.directory)
 
-style = Style(colors=('#00ff00', '#00aa00', '#ff4000', '#ff0000'))
+style = Style(colors=('#00ff00', '#ffa000', '#ff4000', '#ff0000'))
 c = pygal.StackedBar(style=style,  show_minor_x_labels=False, truncate_label=11)
 c.x_labels = uniquify(args.directory)
 c.x_labels_major = list(startify(c.x_labels))
 c.title = 'Stress test results over time'
 c.add('success', [r.cases_ok() for r in results])
-c.add('flaky', [r.cases_fail('flaky') for r in results])
-c.add('inconsistent', [r.cases_fail('inconsistent') for r in results])
-c.add('broken', [r.cases_fail('broken') for r in results])
+c.add('sometimes fail', [r.cases_fail('flaky') for r in results])
+c.add('various fail', [r.cases_fail('inconsistent') for r in results])
+c.add('fail', [r.cases_fail('broken') for r in results])
 import sys, os
 os.write(sys.stdout.fileno(), c.render())
 
