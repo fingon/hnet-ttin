2ffec096d87063f4dc88ae52edc27d11c386da5e
Markus Stenberg <markus.stenberg@iki.fi>
2015-07-01 12:51:53 +0300

    run_stress: Added support for nosetests binary detection.

--- run_stress.sh
+++ run_stress.sh
@@ -8,19 +8,20 @@
 # Copyright (c) 2014 cisco Systems, Inc.
 #
 # Created:       Fri Mar 28 13:15:39 2014 mstenber
-# Last modified: Tue Feb 10 18:58:55 2015 mstenber
-# Edit time:     6 min
+# Last modified: Wed Jul  1 12:51:42 2015 mstenber
+# Edit time:     8 min
 #
 
 # Minor utility script which runs the full topology tests 5 times, and
 # stores the logs.
 
 DIRNAME=${1:-stress}-`date +'%y-%m-%d'`
+NOSETESTS=`which nosetests3` || NOSETESTS=`which nosetests` || exit
 
 mkdir -p $DIRNAME
 for RUN in 1 2 3 4 5
 do
-    nosetests util/test_topo.py 2>&1 | tee $DIRNAME/log$RUN.txt
+    $NOSETESTS util/test_topo.py 2>&1 | tee $DIRNAME/log$RUN.txt
 done
 
 # these are done elsewhere; the stress-* should be moved to safe
